78b0888968cc46a462c076c0d8b27276a54ae1ec
John <powell.john2011@gmail.com>
2019-10-26 15:59:50 +0300

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,9 +9,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  id: number;
-  artistName: string;
-  albumName: string;
+
   products: Product[];
 
   constructor(private _productService: ProductService) { }
